833b6c68eef4fc9d5d7ce4e147d46729802eea87
NAM <kieunhatnamdc@gmail.com>
2022-10-14 17:55:18 +0700

    save bai tap 3

--- NAM/webbanhang-NAM.js
+++
@@ -1,9 +0,0 @@
-var leapYear = function(year){
-    if ((year % 4===0 &&year%100 !==0 && year % 400 !==0)||(year%100===0 && year % 400===0)){
-    return year + " la nam nhuan.";
-    } else{
-    return year + " la nam khong nhuan.";
-    }
-    };
-    var input = prompt("Input year: ");
-    leapYear(input);
